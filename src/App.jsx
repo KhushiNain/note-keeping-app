9f71b6809e196c319c80f03024938fb40d081042
unknown <khushi.nain@kalvium.community>
2024-02-08 11:35:04 +0530

    note-App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Editor from './components/Editor'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Editor/>
+    </>
+  )
+}
+
+export default App
